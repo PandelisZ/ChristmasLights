8a1f9d67e0e259cff4b52387fc05b9264aa25a5b
Pandelis Zembashis <info@pandelis.me>
2015-12-19 10:43:30 +0000

    LED controller

---
+++ lights.js
@@ -0,0 +1,31 @@
+var SerialPort = require("serialport");
+//Set serial port according to Arduino IDE
+var serialPort = new SerialPort.SerialPort("/dev/cu.usbmodem1421", {
+  baudrate: 9600,
+  dataBits: 8,
+  parity: 'none',
+  stopBits: 1,
+  flowControl: false
+});
+
+serialPort.on("open", function () {
+  console.log('serial port open');
+});
+
+var setPixel = function(i, r, g, b) {
+  serialPort.write(i+'I'+r+'R'+g+'G'+b+'BD\n');
+}
+
+var presetPixel = function(i, r, g, b) {
+  serialPort.write(i+'I'+r+'R'+g+'G'+b+'B\n');
+}
+
+var updateStrip = function(i, r, g, b) {
+  serialPort.write('0I0R0G0BD\n');
+}
+
+module.exports = {
+  setPixel: setPixel,
+  presetPixel: presetPixel,
+  updateStrip: updateStrip
+}
